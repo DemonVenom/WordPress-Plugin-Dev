917fe73971c3d893bfd22dea300c4299f628f113
Henry Hoffman <47870005+DemonVenom@users.noreply.github.com>
2022-06-16 12:29:53 -0400

    video 1 progress commit

---
+++ 01/plugins/myplugin/myplugin.php
@@ -0,0 +1,22 @@
+<?php
+
+// disable direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+
+    exit;
+
+}
+
+/*
+Plugin Name:  MyPlugin
+Description:  Example plugin for the video tutorial series, "WordPress: Plugin Development", available at Lynda.com.
+Plugin URI:   https://profiles.wordpress.org/specialk
+Author:       Jeff Starr
+Version:      1.0
+Text Domain:  myplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
